433a42c4fbf2e2d354eb20be259b968c28a5e981
Aditya Yadav <103302549+adiyadav123@users.noreply.github.com>
2024-09-26 22:19:29 +0530

    Update code formatting and fix minor bugs

---
+++ app/page.js
@@ -0,0 +1,54 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+import "./globals.css";
+
+export default function Home() {
+  return (
+    <div className={styles.page}>
+      <h1>Linktree Clone</h1>
+
+      <div className="ln_box">
+        <div className="link_box">
+          <div className="name">Name</div>
+          <div className="inside_link_box">
+            <div className="icon">
+              <div className="dot"></div>
+              <div className="dot"></div>
+              <div className="dot"></div>
+            </div>
+          </div>
+        </div>
+        <div className="link_box">
+        <div className="name">Github</div>
+          <div className="inside_link_box">
+            <div className="icon">
+              <div className="dot"></div>
+              <div className="dot"></div>
+              <div className="dot"></div>
+            </div>
+          </div>
+        </div>
+        <div className="link_box">
+        <div className="name">Youtube</div>
+          <div className="inside_link_box">
+            <div className="icon">
+              <div className="dot"></div>
+              <div className="dot"></div>
+              <div className="dot"></div>
+            </div>
+          </div>
+        </div>
+        <div className="link_box">
+        <div className="name">Instagram</div>
+          <div className="inside_link_box">
+            <div className="icon">
+              <div className="dot"></div>
+              <div className="dot"></div>
+              <div className="dot"></div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
